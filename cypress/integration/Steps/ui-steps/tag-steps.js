42e064639d77b6c615864d827592358d55195231
Andres Camilo Cardenas <liderdesarrollo@tht.company>
2022-11-13 20:49:59 -0500

    Pruebas de Funcionalidad Tags

---
+++ cypress/integration/Steps/ui-steps/tag-steps.js
@@ -0,0 +1,103 @@
+import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+import {tagPage} from "../../../page-objects/tags-page";
+
+const tagPageObj = new tagPage();
+
+
+When(/^a new tag is created$/, function () {
+  tagPageObj.putInputTagName();
+  tagPageObj.putTextAreaTagDescription();
+  tagPageObj.putInputTagColor();
+  tagPageObj.clickToButtonTagExpand();
+  tagPageObj.putInputTagMetaTitle();
+  tagPageObj.putTextAreaTagMetaDescription();
+  tagPageObj.putInputTagCannonicalUrl();
+  tagPageObj.clickToButtonSaveTag();     
+  tagPageObj.clickToButtonLeaveModal();
+});
+
+
+Then(
+  /^verify the title tag$/,
+  function () {
+    tagPageObj.verifyTitleTag();
+  }
+);
+
+And(
+    /^user navigates to the tag page$/,
+    function () {
+        tagPageObj.navigateToTagLink();
+    }
+);
+
+And(
+    /^user click in new tag button$/,
+    function () {
+        tagPageObj.clickToNewTagButton();
+    }
+);
+
+And(
+    /^user click in tag item$/,
+    function () {
+        tagPageObj.clickToLinkFirstTagList();
+    }
+);
+
+And(
+    /^edit title a color text$/,
+    function () {
+        tagPageObj.editColorAndTitleItems();
+    }
+);
+
+And(
+    /^user click in deleted button$/,
+    function () {
+        tagPageObj.clickToButtonTagDelete();
+    }
+);
+
+And(
+    /^user click in reject button$/,
+    function () {
+        tagPageObj.clickToButtonTagRejectModal();
+    }
+);
+
+And(
+    /^user click in accept button$/,
+    function () {
+        tagPageObj.clickToButtonTagAcceptModal();
+    }
+);
+
+And(
+    /^user click in save button$/,
+    function () {
+        tagPageObj.clickToButtonSaveTag();        
+    }
+);
+
+
+Then(
+    /^verify the title tag$/,
+    function () {
+      tagPageObj.verifyNewTitleTag();
+    }
+  );
+
+Then(
+    /^return the tags list$/,
+    function () {
+      tagPageObj.naviageteReturnList();
+    }
+  );
+
+Then(
+    /^user expect to see error$/,
+    function () {
+      tagPageObj.messageError();
+    }
+  );
